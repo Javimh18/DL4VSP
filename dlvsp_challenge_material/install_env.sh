f5b5082620655ab20aa35f0e2805e124a4d49ab6
Javier Muñoz <javier.munnozharo@estudiante.uam.es>
2023-12-31 13:17:55 +0100

    Furst run completed using cuda

--- dlvsp_challenge_material/install_env.sh
+++ dlvsp_challenge_material/install_env.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 conda create --name dlvsp_challenge_env python=3.10
 source activate dlvsp_challenge_env
 
-
 pip3 install numpy
+pip3 install ipykernel
+pip3 install matplotlib
 pip3 install opencv-python
 pip3 install tqdm lap
-pip3 install torch==2.1.0+cu121 torchvision==0.16.0+cu121 torchaudio===2.1.0+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu118
 pip3 install http://www-vpu.eps.uam.es/~jcs/DLVSP/py-motmetrics-fix_pandas_deprecating_warnings_python310.zip
 
 conda install -c conda-forge ipywidgets
